6b325052ca9c552f42f80c3f0fcce74000ed8c20
HasegawaNaoto <58247519+HasegawaNaoto@users.noreply.github.com>
2020-02-18 22:13:02 +0900

    Update background_uploader.rb

--- app/uploaders/background_uploader.rb
+++ app/uploaders/background_uploader.rb
@@ -58,33 +58,19 @@ class BackgroundUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-  #上限変更
   process :resize_to_limit => [700, 700]
 
-  #JPGで保存
   process :convert => 'jpg'
 
-  #サムネイルを生成
   version :thumb do
     process :resize_to_limit => [300, 300]
   end
 
-  # jpg,jpeg,gif,pngのみ
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  #ファイル名を変更し拡張子を同じにする
+  
   def filename
     super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
   end
-
-  #日付で保存
-  # def filename
-  #   if original_filename.present?
-  #     time = Time.now
-  #     name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
-  #     name.downcase
-  #   end
-  # end
 end
